51218e6402d71397252abf863507679fed332005
Otavio757 <otavioazevedo@outlook.com.br>
2020-06-14 02:36:09 -0300

    Implementação da exceção de moedas insuficientes para o troco

---
+++ Desafio-Compuletra/Exceptions/InsufficientCoinsToChangeException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio_Compuletra.Exceptions
+{
+    class InsufficientCoinsToChangeException : Exception
+    {
+        public double TotalValue { get; private set; } //Valor total do troco
+        public double MissingValue { get; private set; } //Valor que faltou
+
+        public InsufficientCoinsToChangeException(double totalValue, double missingValue)
+        {
+            TotalValue = totalValue;
+            MissingValue = missingValue;
+        }
+
+        public override string ToString()
+        {
+            return "Não há moedas suficientes para o troco de $" + TotalValue + ", faltam $" + MissingValue;
+        }
+    }
+}
---
+++ Desafio-Compuletra/Exceptions/InsufficientCoinsToWithdrawException.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio_Compuletra.Exceptions
+{
+    class InsufficientCoinsToWithdrawException : Exception
+    {
+        public int CoinsAvailable { get; private set; }
+        public int IntendedWithdraw { get; private set; }
+        public double CoinValue { get; private set; }
+
+        public InsufficientCoinsToWithdrawException(int coinsAvailable, int intendedWithdraw, double coinValue)
+        {
+            CoinsAvailable = coinsAvailable;
+            IntendedWithdraw = intendedWithdraw;
+            CoinValue = coinValue;
+        }
+
+        public override string ToString()
+        {
+            return "Houve uma tentativa de saque de " + IntendedWithdraw + " moedas de $" + CoinValue + ", porém há somente " + CoinsAvailable + " moedas desse valor";
+        }
+    }
+}
